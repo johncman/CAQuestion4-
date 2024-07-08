83c7b1473c73764557d160eee14aef2223ee9c6d
johncman <johncaulfield374@gmail.com>
2024-07-08 13:33:14 +0000

    CA Question 4

--- main.cpp
+++ main.cpp
@@ -6,18 +6,14 @@ int main() {
     int b = 20;
     int c = 30;
 
-    // Add values of a and b, store in a new int variable d
+    // Add values of a and b, store it in a new int variable d and print value of variable d
     int d = a + b;
     // Print value of variable d
     printf("Value of d (a + b) = %d\n", d);
 
-    // Subtract variable c from d, store in a new int variable e
+    // Subtract variable c from d and store in new int variable e and print the value of variable e
     int e = d - c;
     // Print value of variable e
     printf("Value of e (d - c) = %d\n", e);
 
-    // Keep the program running
-    while (true) {
-        // Continue loop
-    }
 }
